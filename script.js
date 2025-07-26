75ced029b5c01b7b4108606d322eb53f28fea1af
Jam <jameskuyt655@gmail.com>
2025-07-26 17:01:24 +0000

    Refactor index.html and script.js: improve text clarity and restore showVideo function

---
+++ script.js
@@ -0,0 +1,3 @@
+ function showVideo() {
+            window.open('https://teslakitty-cdn.netlify.app/cat-tube/chloe.mp4', 'catVideo', 'width=800,height=600,toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=yes');
+        }
